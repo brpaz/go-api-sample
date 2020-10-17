f5db8aa5d1286c184a28768ab9252d57a6006bf1
Bruno Paz <brunopaz@sapo.pt>
2020-10-17 15:54:12 +0100

    fix test

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -13,9 +13,18 @@ import (
 func TestLoad(t *testing.T) {
 
 	_ = os.Setenv("APP_PORT", "1000")
+	_ = os.Setenv("DB_HOST", "localhost")
+	_ = os.Setenv("DB_PORT", "5432")
+	_ = os.Setenv("DB_USER", "postgres")
+	_ = os.Setenv("DB_PASSWORD", "123456")
 	_ = os.Unsetenv("APP_DEBUG")
+
 	defer func() {
 		_ = os.Unsetenv("APP_PORT")
+		_ = os.Unsetenv("DB_HOST")
+		_ = os.Unsetenv("DB_PORT")
+		_ = os.Unsetenv("DB_USER")
+		_ = os.Unsetenv("DB_PASSWORD")
 	}()
 
 	cfg, err := config.Load()
